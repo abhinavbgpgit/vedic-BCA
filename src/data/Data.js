e9c8e7c42bb05f4e3377cd0f075a41d839f3fd2b
Akashkumar9572 <--global>
2024-02-24 10:07:57 +0530

    added data

---
+++ src/data/Data.js
@@ -0,0 +1,57 @@
+const universities_Name=[
+    {
+        "name": "Babasaheb Bhimrao Ambedkar Bihar University",
+        "abbreviation": "BRABU",
+        "location": "Muzaffarpur"
+      },
+      {
+        "name": "Chanakya National Law University",
+        "abbreviation": "CNLU",
+        "location": "Patna"
+      },
+      {
+        "name": "Jai Prakash Vishwavidyalaya",
+        "abbreviation": "JPV",
+        "location": "Chapra"
+      },
+      {
+        "name": "Lalit Narayan Mithila University",
+        "abbreviation": "LNMU",
+        "location": "Darbhanga"
+      },
+      {
+        "name": "Magadh University",
+        "abbreviation": "MU",
+        "location": "Bodh Gaya"
+      },
+      {
+        "name": "Maulana Mazharul Haque Arabic and Persian University",
+        "abbreviation": "MMHAPU",
+        "location": "Patna"
+      },
+      {
+        "name": "Nalanda Open University",
+        "abbreviation": "NOU",
+        "location": "Patna"
+      },
+      {
+        "name": "Patna University",
+        "abbreviation": "PU",
+        "location": "Patna"
+      },
+      {
+        "name": "Rajendra Agricultural University",
+        "abbreviation": "RAU",
+        "location": "Pusa, Samastipur"
+      },
+      {
+        "name": "Tilka Manjhi Bhagalpur University",
+        "abbreviation": "TMBU",
+        "location": "Bhagalpur"
+      },
+      {
+        "name": "Veer Kunwar Singh University",
+        "abbreviation": "VKSU",
+        "location": "Ara"
+      }
+]
