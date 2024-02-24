06d5d66e5e76780240bf8d61a9967af04b8e4b1a
Kislay Raj <kislayraj751@gmail.com>
2024-02-24 09:46:48 +0530

    add

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React, come to  Vedic technologies.
         </a>
       </header>
     </div>
