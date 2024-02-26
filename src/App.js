b1b7e1ad30b1f2af65a46ac58663a0a169f2bbd7
Kislay Raj <kislayraj751@gmail.com>
2024-02-26 19:08:41 +0530

    modified navbar

--- src/App.js
+++ src/App.js
@@ -1,40 +1,29 @@
-import Navbar from './components/Navbar';
-import logo from './logo.svg';
-import Campus from './pages/Campus';
-import Events from './pages/Events';
-import Home from './pages/Home';
-import { Routes,Route } from 'react-router-dom';
-import Syllabus from './pages/Syllabus';
-import Result from './pages/Result';
-import ExamPage from './pages/ExamPage';
+import Navbar from "../src/components/Navbar";
+
+import logo from "./logo.svg";
+import Campus from "./pages/Campus";
+import Events from "./pages/Events";
+import Home from "./pages/Home";
+import { Routes, Route } from "react-router-dom";
+import Syllabus from "./pages/Syllabus";
+import Result from "./pages/Result";
+import ExamPage from "./pages/ExamPage";
 
 function App() {
   // amanraj
   return (
-<<<<<<< HEAD
     <>
-    
+      <Navbar />
 
-
-    <Navbar/> 
-
-     <Routes>
-      <Route path="/" element={<Home/>}></Route>
-      <Route path="/campus" element={<Campus/>}></Route>
-      <Route path="/events" element={<Events/>}></Route>
-      <Route path="/syllabus" element={<Syllabus/>}></Route>
-      <Route path="/result" element={<Result/>}></Route>
-      <Route path="/exam" element={<ExamPage/>}></Route>
-     </Routes>
-    
-    
-
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/campus" element={<Campus />}></Route>
+        <Route path="/events" element={<Events />}></Route>
+        <Route path="/syllabus" element={<Syllabus />}></Route>
+        <Route path="/result" element={<Result />}></Route>
+        <Route path="/exam" element={<ExamPage />}></Route>
+      </Routes>
     </>
-=======
-    <div className="App ">
-     hello
-    </div>
->>>>>>> 3fc18693a3e1502b67e2d3c3a7f7447a1e6b09d5
   );
 }
 
