df76ebff3aa2f37b6fdef34e1bc66903920c74fe
abhinavbgpgit <abhinavbgp@gmail.com>
2024-02-27 15:14:15 +0530

    ok

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,95 +1,69 @@
 import React from 'react'
+import { Button } from 'flowbite-react';
+import { Spinner } from 'flowbite-react';
 import { Label, Select } from 'flowbite-react';
-import { Link } from 'react-router-dom';
 const Home = () => {
   return (
-
-    <div className="center h-screen flex-col">
-   <Link to='/exam'><div className="">Go to exam page</div></Link> 
-    <div className="text-5xl">Welcome to <span className='text-blue-400'>Vedic Bihar</span> </div>
+    <div className="flex justify-start h-screen flex-col items-center">
     
+    <div className="text-3xl md:text-5xl  mt-[5%] ">Welcome to <span className='text-blue-400'>Vedic Bihar</span>  </div>
+    {/* <Button className='mt-3'>Click me</Button> */}
    
-    <div className='w-[500px] mt-10'>
+
+    <div className="w-[400px] mt-20">
       <div className="mb-2 block">
         <Label htmlFor="countries" value="Select your University" />
       </div>
-      <Select id="university" required>
-      <option value="BRABU">Babasaheb Bhimrao Ambedkar Bihar University</option>
-  <option value="CNLU">Chanakya National Law University</option>
-  <option value="JPV">Jai Prakash Vishwavidyalaya</option>
-  <option value="LNMU">Lalit Narayan Mithila University</option>
-  <option value="MU">Magadh University</option>
-  <option value="MMHAPU">Maulana Mazharul Haque Arabic and Persian University</option>
-  <option value="NOU">Nalanda Open University</option>
-  <option value="PU">Patna University</option>
-  <option value="RAU">Rajendra Agricultural University</option>
-  <option value="TMBU">Tilka Manjhi Bhagalpur University</option>
-  <option value="VKSU">Veer Kunwar Singh University</option>
+      <Select id="countries" required>
+        <option>Tilka Manjhi Bhagalpur University</option>
+        <option>Veer Kunwar Singh University</option>
+        <option>Rajendra Agricultural University"</option>
+        <option>Patna University</option>
+        <option value="">Maulana Mazharul Haque Arabic and Persian University</option>
+        <option value="">Magadh University</option>
+        <option value="">Lalit Narayan Mithila University</option>
+        <option value="">Jai Prakash Vishwavidyalaya</option>
       </Select>
-    </div>
-
-    <div className='w-[500px] mt-5'>
+      <div className="w-[400px] mt-5">
       <div className="mb-2 block">
-        <Label htmlFor="countries" value="Select your College" />
+        <Label htmlFor="countries" value="Select your University" />
       </div>
-      <Select id="university" required>
-      <option value="BRABU">Marwari College</option>
-  <option value="CNLU">T. N. B. College</option>
-  <option value="JPV">S.M. College</option>
-  <option value="LNMU">Mahadev Singh College</option>
-  <option value="MU">Magadh University</option>
-  <option value="MMHAPU">Maulana Mazharul Haque Arabic and Persian University</option>
-  <option value="NOU">Nalanda Open University</option>
-  <option value="PU">Patna University</option>
-  <option value="RAU">Rajendra Agricultural University</option>
-  <option value="TMBU">Tilka Manjhi Bhagalpur University</option>
-  <option value="VKSU">Veer Kunwar Singh University</option>
-      </Select>
+      <Select id="colleges" required>
+  <option>T.N.B. College, Bhagalpur</option>
+  <option>S.P. Jain College, Bhagalpur</option>
+  <option>Marwari College, Bhagalpur</option>
+  <option>J.P. College, Narayanpur</option>
+  <option>Murarka College, Sultanganj</option>
+  <option>S.S. Vidyapeeth, Deoghar</option>
+  <option>P.B.S. College, Banka</option>
+  <option>Bhagalpur National College, Bhagalpur</option>
+  <option>T.N.B. Law College, Bhagalpur</option>
+  <option>Sabour College, Sabour</option>
+  <option>G.B. College, Naugachia</option>
+  <option>Jamalpur College, Jamalpur</option>
+  <option>B.N.M. College, Barhiya</option>
+  <option>B.N.S. College, Barhiya</option>
+  <option>K.D. College, Gogri</option>
+  <option>M.S. College, Barhariya</option>
+  <option>B.N.M. College, Bhagalpur</option>
+  <option>B.N.S. College, Bhagalpur</option>
+  <option>Koshi College, Khagaria</option>
+  <option>S.M. College, Bhagalpur</option>
+  <option>G.B. College, Gopalpur</option>
+  <option>R.N.B. College, Navgachhia</option>
+  <option>S.K. Raj College, Katihar</option>
+  <option>R.D. & D.J. College, Munger</option>
+  <option>T.N.B. Commerce College, Bhagalpur</option>
+  <option>Madan Ahilya Mahila College, Naugachia</option>
+  <option>S.S.V. College, Kahalgaon</option>
+  <option>B.L.S.G. College, Maheshpur</option>
+  <option>J.M.S. College, Munger</option>
+  <option>B.N.S. Mahila College, Madhepura</option>
+</Select>
+</div>
     </div>
-
-
-    <div className=" flex gap-5">
-    <div className='w-[240px] mt-5'>
-      <div className="mb-2 block">
-        <Label htmlFor="countries" value="Select your Department" />
-      </div>
-      <Select id="university" required>
-      <option value="BRABU">BCA</option>
-  <option value="CNLU">BBA</option>
-  <option value="JPV">BIT</option>
-  <option value="LNMU">BSc</option>
-  <option value="MU">BCom</option>
-  <option value="MMHAPU">Maulana Mazharul Haque Arabic and Persian University</option>
-  <option value="NOU">Nalanda Open University</option>
-  <option value="PU">Patna University</option>
-  <option value="RAU">Rajendra Agricultural University</option>
-  <option value="TMBU">Tilka Manjhi Bhagalpur University</option>
-  <option value="VKSU">Veer Kunwar Singh University</option>
-      </Select>
-    </div>
-    <div className='w-[240px] mt-5'>
-      <div className="mb-2 block">
-        <Label htmlFor="countries" value="Select your Semester/Year" />
-      </div>
-      <Select id="university" required>
-      <option value="1st Sem">1st Sem</option>
-  <option value="CNLU">2nd sem</option>
-  <option value="JPV">3rd sem</option>
-  <option value="LNMU">4th sem</option>
-  <option value="MU">5th sem</option>
-  <option value="MMHAPU">6th sem</option>
-  <option value="NOU">7th sem</option>
-  <option value="PU">8th sem</option>
-  <option value="RAU">1st year</option>
-  <option value="TMBU">2nd year</option>
-  <option value="VKSU">3rd year</option>
- 
-      </Select>
-    </div>
-    </div>
-
-
-        </div> 
+    
+        </div>
   )
 }
 
